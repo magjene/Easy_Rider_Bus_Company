022060f5a3cddb446ad6d41560500e0fa0e1604c
magjene <pdfjene@mail.ru>
2022-09-26 13:53:14 +0300

    Added DOC Changed s

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,26 +1,28 @@
 """
 Project: Easy Rider Bus Company
-Stage 1/6: Checking the data type
+Stage 2/6: Correct syntax
 
 
 Description
-You just started sorting out the existing database of the "Easy Rider" bus company. As you take the first look at the data, you realize that it's not going to be easy.
+You managed to fill in all the missing data and correct the mistakes with the types. However, you noticed that there are multiple problems with suffix names for the stops: sometimes they are incorrect, and sometimes they are simply missing. As if that was not enough, you also realized that there are errors in the arrival times.
 
-Sometimes numbers are missing from where they should definitely be. You also noticed that sometimes there are too many or too few characters. Fortunately, there is documentation to help you sort out this mess. However, this documentation is not a hundred percent complete: part of it was torn away when your colleague spilled coffee on it. Let's see what we can make out.
+It seems like you have to carefully look at the entire "Format" column in the first part of the documentation.
 
 Here are the documents that you have: documentation and diagram of the bus lines.
 
 Objectives
 The string containing the data in JSON format is passed to standard input.
-Check that the data types match.
-Check that the required fields are filled in.
-Display the information about the number of found errors in total and in each field. Keep in mind that there might be no errors at all.
+Check that the data format complies with the documentation.
+Only the fields that have such a requirement are relevant, i.e. stop_name, stop_type, a_time, so, please, count errors only for them.
+Like in the previous stage, print the information about the number of found errors in total and in each field. Remember that there might be no errors at all.
 The output should have the same formatting as shown in the example.
-No need to worry about the format now. Let's at first just make sure that the fields have the right type and all required ones are filled.
 If you can't find the necessary information in the stage description, it can probably be found in the attached documentation.
 
-Note that the type Char is present among the data types.
+Note that the time format is military time (24 hours, hh:mm). That means that there are certain restrictions:
 
+the first digit cannot be 3, 4, etc.;
+hours less than 10 should have zero in front of them, e.g. 08:34;
+the delimiter should be colon :.
 Example
 Input:
 
@@ -28,47 +30,47 @@ Input:
     {
         "bus_id": 128,
         "stop_id": 1,
-        "stop_name": "Prospekt Avenue",
+        "stop_name": "Prospekt Av.",
         "next_stop": 3,
         "stop_type": "S",
-        "a_time": 8.12
+        "a_time": "08:12"
     },
     {
         "bus_id": 128,
         "stop_id": 3,
-        "stop_name": "",
+        "stop_name": "Elm Street",
         "next_stop": 5,
         "stop_type": "",
-        "a_time": "08:19"
+        "a_time": "8:19"
     },
     {
         "bus_id": 128,
         "stop_id": 5,
         "stop_name": "Fifth Avenue",
         "next_stop": 7,
-        "stop_type": "O",
+        "stop_type": "OO",
         "a_time": "08:25"
     },
     {
         "bus_id": 128,
-        "stop_id": "7",
+        "stop_id": 7,
         "stop_name": "Sesame Street",
         "next_stop": 0,
         "stop_type": "F",
-        "a_time": "08:37"
+        "a_time": "08:77"
     },
     {
-        "bus_id": "",
+        "bus_id": 256,
         "stop_id": 2,
         "stop_name": "Pilotow Street",
         "next_stop": 3,
         "stop_type": "S",
-        "a_time": ""
+        "a_time": "09:20"
     },
     {
         "bus_id": 256,
         "stop_id": 3,
-        "stop_name": "Elm Street",
+        "stop_name": "Elm",
         "next_stop": 6,
         "stop_type": "",
         "a_time": "09:45"
@@ -78,43 +80,40 @@ Input:
         "stop_id": 6,
         "stop_name": "Sunset Boulevard",
         "next_stop": 7,
-        "stop_type": "",
+        "stop_type": "A",
         "a_time": "09:59"
     },
     {
         "bus_id": 256,
         "stop_id": 7,
         "stop_name": "Sesame Street",
-        "next_stop": "0",
+        "next_stop": 0,
         "stop_type": "F",
-        "a_time": "10:12"
+        "a_time": "10.12"
     },
     {
         "bus_id": 512,
         "stop_id": 4,
-        "stop_name": "Bourbon Street",
+        "stop_name": "bourbon street",
         "next_stop": 6,
         "stop_type": "S",
-        "a_time": "08:13"
+        "a_time": "38:13"
     },
     {
-        "bus_id": "512",
+        "bus_id": 512,
         "stop_id": 6,
         "stop_name": "Sunset Boulevard",
         "next_stop": 0,
-        "stop_type": 5,
+        "stop_type": "F",
         "a_time": "08:16"
     }
 ]
 Output:
 
-Type and required field validation: 8 errors
-bus_id: 2
-stop_id: 1
-stop_name: 1
-next_stop: 1
-stop_type: 1
-a_time: 2
+Format validation: 9 errors
+stop_name: 3
+stop_type: 2
+a_time: 4
 """
 
 
@@ -141,8 +140,8 @@ def time(k_j, k_e, v_j):
 err_dict = {'bus_id': [0, 'int'], 'stop_id': [0, 'int'], 'stop_name': [0, 'str'], 'next_stop': [0, 'int'],
             'stop_type': [0, 'char'], 'a_time': [0, 'time']}
 
-# s = '[{"bus_id": 128, "stop_id": 1, "stop_name": "Prospekt Avenue", "next_stop": 3, "stop_type": "S", "a_time": 8.12}, {"bus_id": 128, "stop_id": 3, "stop_name": "", "next_stop": 5, "stop_type": "", "a_time": "08:19"}, {"bus_id": 128, "stop_id": 5, "stop_name": "Fifth Avenue", "next_stop": 7, "stop_type": "O", "a_time": "08:25"}, {"bus_id": 128, "stop_id": "7", "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "08:37"}, {"bus_id": "", "stop_id": 2, "stop_name": "Pilotow Street", "next_stop": 3, "stop_type": "S", "a_time": ""}, {"bus_id": 256, "stop_id": 3, "stop_name": "Elm Street", "next_stop": 6, "stop_type": "", "a_time": "09:45"}, {"bus_id": 256, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 7, "stop_type": "", "a_time": "09:59"}, {"bus_id": 256, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": "0", "stop_type": "F", "a_time": "10:12"}, {"bus_id": 512, "stop_id": 4, "stop_name": "Bourbon Street", "next_stop": 6, "stop_type": "S", "a_time": "08:13"}, {"bus_id": "512", "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 0, "stop_type": 5, "a_time": "08:16"}]'
-s = input()
+s = '[{"bus_id": 128, "stop_id": 1, "stop_name": "Prospekt Av.", "next_stop": 3, "stop_type": "S", "a_time": "08:12"}, {"bus_id": 128, "stop_id": 3, "stop_name": "Elm Street", "next_stop": 5, "stop_type": "", "a_time": "8:19"}, {"bus_id": 128, "stop_id": 5, "stop_name": "Fifth Avenue", "next_stop": 7, "stop_type": "OO", "a_time": "08:25"}, {"bus_id": 128, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "08:77"}, {"bus_id": 256, "stop_id": 2, "stop_name": "Pilotow Street", "next_stop": 3, "stop_type": "S", "a_time": "09:20"}, {"bus_id": 256, "stop_id": 3, "stop_name": "Elm", "next_stop": 6, "stop_type": "", "a_time": "09:45"}, {"bus_id": 256, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 7, "stop_type": "A", "a_time": "09:59"}, {"bus_id": 256, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "10.12"}, {"bus_id": 512, "stop_id": 4, "stop_name": "bourbon street", "next_stop": 6, "stop_type": "S", "a_time": "38:13"}, {"bus_id": 512, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 0, "stop_type": "F", "a_time": "08:16"}]'
+# s = input()
 json_list = loads(s)
 for json_dict in json_list:
     for j, e in zip(json_dict.items(), err_dict.items()):
