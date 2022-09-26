5f5b0bfd27019eb4c1ae63df4259a46005ea066d
magjene <pdfjene@mail.ru>
2022-09-26 13:07:35 +0300

    Fixed s

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -141,9 +141,9 @@ def time(k_j, k_e, v_j):
 err_dict = {'bus_id': [0, 'int'], 'stop_id': [0, 'int'], 'stop_name': [0, 'str'], 'next_stop': [0, 'int'],
             'stop_type': [0, 'char'], 'a_time': [0, 'time']}
 
-s = input()
+s = '[{"bus_id": 128, "stop_id": 1, "stop_name": "Prospekt Avenue", "next_stop": 3, "stop_type": "S", "a_time": 8.12}, {"bus_id": 128, "stop_id": 3, "stop_name": "", "next_stop": 5, "stop_type": "", "a_time": "08:19"}, {"bus_id": 128, "stop_id": 5, "stop_name": "Fifth Avenue", "next_stop": 7, "stop_type": "O", "a_time": "08:25"}, {"bus_id": 128, "stop_id": "7", "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "08:37"}, {"bus_id": "", "stop_id": 2, "stop_name": "Pilotow Street", "next_stop": 3, "stop_type": "S", "a_time": ""}, {"bus_id": 256, "stop_id": 3, "stop_name": "Elm Street", "next_stop": 6, "stop_type": "", "a_time": "09:45"}, {"bus_id": 256, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 7, "stop_type": "", "a_time": "09:59"}, {"bus_id": 256, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": "0", "stop_type": "F", "a_time": "10:12"}, {"bus_id": 512, "stop_id": 4, "stop_name": "Bourbon Street", "next_stop": 6, "stop_type": "S", "a_time": "08:13"}, {"bus_id": "512", "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 0, "stop_type": 5, "a_time": "08:16"}]'
+# s = input()
 json_dict = loads(s)
-
 for i in range(len(json_dict)):
     for j, e in zip(json_dict[i].items(), err_dict.items()):
         key_e, val_e, key_j, val_j = *e, *j
