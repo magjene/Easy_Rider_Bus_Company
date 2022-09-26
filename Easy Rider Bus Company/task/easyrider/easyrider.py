c3361a6adf5c8cd05575a165fb7c199e0a1c6f92
magjene <pdfjene@mail.ru>
2022-09-26 19:37:12 +0300

    Added DOC

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,22 +1,85 @@
 """
 Project: Easy Rider Bus Company
-Stage 3/6: Bus line info
+Stage 4/6: Special stops
 
 
 Description
-It wasn't easy, but finally, you verified the data format and the required fields. It is now time to check how many bus lines we have and how many stops there are on each line. Before we can go further with sorting out the database, it would be a good idea to check that the information is complete.
+The missing part of the specification is nowhere to be found. You need to recover and update the data contained in the second part of the documentation.
+
+The company is growing, the number of bus lines is ever-increasing, and logically, more bus stops appear. You need to prepare an appropriate function that calculates this data so that it doesn't have to be checked manually in the future.
 
 Here are the documents that you have: documentation and diagram of the bus lines.
 
 Objectives
 The string containing the data in JSON format is passed to standard input.
-Find the names of all the bus lines.
-Verify the number of stops for each line.
+Make sure each bus line has exactly one starting point (S) and one final stop (F).
+If a bus line does not meet this condition, stop checking and print a message about it. Do not continue checking the other bus lines.
+If all bus lines meet the condition, count how many starting points and final stops there are. Print their unique names in alphabetical order.
+Count the transfer stops and print their unique names in alphabetical order. A transfer stop is a stop shared by at least two bus lines.
 The output should have the same formatting as shown in the example.
-If you can't find the necessary information in the stage description, it can probably be found in the attached documentation.
+If you cannot find the necessary information in the stage description, it can probably be found in the attached documentation.
 
-Example
-Input:
+Examples
+Example 1
+
+Input 1:
+
+[
+    {
+        "bus_id": 128,
+        "stop_id": 1,
+        "stop_name": "Prospekt Avenue",
+        "next_stop": 3,
+        "stop_type": "S",
+        "a_time": "08:12"
+    },
+    {
+        "bus_id": 128,
+        "stop_id": 3,
+        "stop_name": "Elm Street",
+        "next_stop": 5,
+        "stop_type": "",
+        "a_time": "08:19"
+    },
+    {
+        "bus_id": 128,
+        "stop_id": 5,
+        "stop_name": "Fifth Avenue",
+        "next_stop": 7,
+        "stop_type": "O",
+        "a_time": "08:25"
+    },
+    {
+        "bus_id": 128,
+        "stop_id": 7,
+        "stop_name": "Sesame Street",
+        "next_stop": 0,
+        "stop_type": "F",
+        "a_time": "08:37"
+    },
+    {
+        "bus_id": 512,
+        "stop_id": 4,
+        "stop_name": "Bourbon Street",
+        "next_stop": 6,
+        "stop_type": "",
+        "a_time": "08:13"
+    },
+    {
+        "bus_id": 512,
+        "stop_id": 6,
+        "stop_name": "Sunset Boulevard",
+        "next_stop": 0,
+        "stop_type": "F",
+        "a_time": "08:16"
+    }
+]
+Output 1:
+
+There is no start or end stop for the line: 512.
+Example 2
+
+Input 2:
 
 [
     {
@@ -100,12 +163,11 @@ Input:
         "a_time": "08:16"
     }
 ]
-Output:
+Output 2:
 
-Line names and number of stops:
-bus_id: 128, stops: 4
-bus_id: 256, stops: 4
-bus_id: 512, stops: 2
+Start stops: 3 ['Bourbon Street', 'Pilotow Street', 'Prospekt Avenue']
+Transfer stops: 3 ['Elm Street', 'Sesame Street', 'Sunset Boulevard']
+Finish stops: 2 ['Sesame Street', 'Sunset Boulevard']
 """
 
 
@@ -122,8 +184,6 @@ err_dict = {'bus_id': [0, 'int'],
 
 bus_stop = {}
 
-# s = '[{"bus_id": 128,"stop_id": 1,"stop_name": "Prospekt Avenue","next_stop": 3,"stop_type": "S","a_time": "08:12"}, {"bus_id": 128,"stop_id": 3,"stop_name": "Elm Street","next_stop": 5,"stop_type": "","a_time": "08:19"}, {"bus_id": 128,"stop_id": 5,"stop_name": "Fifth Avenue","next_stop": 7,"stop_type": "O","a_time": "08:25"}, {"bus_id": 128,"stop_id": 7,"stop_name": "Sesame Street","next_stop": 0,"stop_type": "F","a_time": "08:37"}, {"bus_id": 256,"stop_id": 2,"stop_name": "Pilotow Street","next_stop": 3,"stop_type": "S","a_time": "09:20"}, {"bus_id": 256,"stop_id": 3,"stop_name": "Elm Street","next_stop": 6,"stop_type": "","a_time": "09:45"}, {"bus_id": 256,"stop_id": 6,"stop_name": "Sunset Boulevard","next_stop": 7,"stop_type": "","a_time": "09:59"}, {"bus_id": 256,"stop_id": 7,"stop_name": "Sesame Street","next_stop": 0,"stop_type": "F","a_time": "10:12"}, {"bus_id": 512,"stop_id": 4,"stop_name": "Bourbon Street","next_stop": 6,"stop_type": "S","a_time": "08:13"}, {"bus_id": 512,"stop_id": 6,"stop_name": "Sunset Boulevard","next_stop": 0,"stop_type": "F","a_time": "08:16"}]'
-# s = '[{"bus_id" : 128, "stop_id" : 1, "stop_name" : "Fifth Avenue", "next_stop" : 4, "stop_type" : "S", "a_time" : "08:12"}, {"bus_id" : 128, "stop_id" : 4, "stop_name" : "Abbey Road", "next_stop" : 5, "stop_type" : "", "a_time" : "08:19"},  {"bus_id" : 128, "stop_id" : 5, "stop_name" : "Santa Monica Boulevard", "next_stop" : 8, "stop_type" : "O", "a_time" : "08:25"},  {"bus_id" : 128, "stop_id" : 8, "stop_name" : "Elm Street", "next_stop" : 11, "stop_type" : "", "a_time" : "08:37"},  {"bus_id" : 128, "stop_id" : 11, "stop_name" : "Beale Street", "next_stop" : 12, "stop_type" : "", "a_time" : "09:20"},  {"bus_id" : 128, "stop_id" : 12, "stop_name" : "Sesame Street", "next_stop" : 14, "stop_type" : "", "a_time" : "09:45"},  {"bus_id" : 128, "stop_id" : 14, "stop_name" : "Bourbon Street", "next_stop" : 19, "stop_type" : "O", "a_time" : "09:59"},  {"bus_id" : 128, "stop_id" : 19, "stop_name" : "Prospekt Avenue", "next_stop" : 0, "stop_type" : "F", "a_time" : "10:12"},  {"bus_id" : 256, "stop_id" : 2, "stop_name" : "Pilotow Street", "next_stop" : 3, "stop_type" : "S", "a_time" : "08:13"},  {"bus_id" : 256, "stop_id" : 3, "stop_name" : "Startowa Street", "next_stop" : 8, "stop_type" : "", "a_time" : "08:16"},  {"bus_id" : 256, "stop_id" : 8, "stop_name" : "Elm Street", "next_stop" : 10, "stop_type" : "", "a_time" : "08:29"},  {"bus_id" : 256, "stop_id" : 10, "stop_name" : "Lombard Street", "next_stop" : 12, "stop_type" : "", "a_time" : "08:44"},  {"bus_id" : 256, "stop_id" : 12, "stop_name" : "Sesame Street", "next_stop" : 13, "stop_type" : "O", "a_time" : "08:46"},  {"bus_id" : 256, "stop_id" : 13, "stop_name" : "Orchard Road", "next_stop" : 16, "stop_type" : "", "a_time" : "09:13"}, {"bus_id" : 256, "stop_id" : 16, "stop_name" : "Sunset Boulevard", "next_stop" : 17, "stop_type" : "O", "a_time" : "09:26"},  {"bus_id" : 256, "stop_id" : 17, "stop_name" : "Khao San Road", "next_stop" : 20, "stop_type" : "O", "a_time" : "10:25"},  {"bus_id" : 256, "stop_id" : 20, "stop_name" : "Michigan Avenue", "next_stop" : 0, "stop_type" : "F", "a_time" : "11:26"},  {"bus_id" : 512, "stop_id" : 6, "stop_name" : "Arlington Road", "next_stop" : 7, "stop_type" : "S", "a_time" : "11:06"},  {"bus_id" : 512, "stop_id" : 7, "stop_name" : "Parizska Street", "next_stop" : 8, "stop_type" : "", "a_time" : "11:15"},  {"bus_id" : 512, "stop_id" : 8, "stop_name" : "Elm Street", "next_stop" : 9, "stop_type" : "", "a_time" : "11:56"},  {"bus_id" : 512, "stop_id" : 9, "stop_name" : "Niebajka Avenue", "next_stop" : 15, "stop_type" : "", "a_time" : "12:20"},  {"bus_id" : 512, "stop_id" : 15, "stop_name" : "Jakis Street", "next_stop" : 16, "stop_type" : "", "a_time" : "12:44"},  {"bus_id" : 512, "stop_id" : 16, "stop_name" : "Sunset Boulevard", "next_stop" : 18, "stop_type" : "", "a_time" : "13:01"},  {"bus_id" : 512, "stop_id" : 18, "stop_name" : "Jakas Avenue", "next_stop" : 19, "stop_type" : "", "a_time" : "14:00"},  {"bus_id" : 1024, "stop_id" : 21, "stop_name" : "Karlikowska Avenue", "next_stop" : 12, "stop_type" : "S", "a_time" : "13:01"},  {"bus_id" : 1024, "stop_id" : 12, "stop_name" : "Sesame Street", "next_stop" : 0, "stop_type" : "F", "a_time" : "14:00"},  {"bus_id" : 512, "stop_id" : 19, "stop_name" : "Prospekt Avenue", "next_stop" : 0, "stop_type" : "F", "a_time" : "14:11"}]'
 s = input()
 json_list = loads(s)
 for json_dict in json_list:
