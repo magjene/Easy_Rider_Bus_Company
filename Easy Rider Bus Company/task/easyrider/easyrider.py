45f1f58dc2c6071f5170ef0b70c758a3f5c4b0d0
magjene <pdfjene@mail.ru>
2022-09-26 12:41:57 +0300

    Fixed s

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -121,97 +121,6 @@ a_time: 2
 from json import loads
 
 
-err_dict = {'bus_id': [0, 'int'], 'stop_id': [0, 'int'], 'stop_name': [0, 'str'], 'next_stop': [0, 'int'],
-            'stop_type': [0, 'char'], 'a_time': [0, 'time']}
-
-
-s = input()
-
-
-# s = '''[
-#     {
-#         "bus_id": 128,
-#         "stop_id": 1,
-#         "stop_name": "Prospekt Avenue",
-#         "next_stop": 3,
-#         "stop_type": "S",
-#         "a_time": 8.12
-#     },
-#     {
-#         "bus_id": 128,
-#         "stop_id": 3,
-#         "stop_name": "",
-#         "next_stop": 5,
-#         "stop_type": "",
-#         "a_time": "08:19"
-#     },
-#     {
-#         "bus_id": 128,
-#         "stop_id": 5,
-#         "stop_name": "Fifth Avenue",
-#         "next_stop": 7,
-#         "stop_type": "O",
-#         "a_time": "08:25"
-#     },
-#     {
-#         "bus_id": 128,
-#         "stop_id": "7",
-#         "stop_name": "Sesame Street",
-#         "next_stop": 0,
-#         "stop_type": "F",
-#         "a_time": "08:37"
-#     },
-#     {
-#         "bus_id": "",
-#         "stop_id": 2,
-#         "stop_name": "Pilotow Street",
-#         "next_stop": 3,
-#         "stop_type": "S",
-#         "a_time": ""
-#     },
-#     {
-#         "bus_id": 256,
-#         "stop_id": 3,
-#         "stop_name": "Elm Street",
-#         "next_stop": 6,
-#         "stop_type": "",
-#         "a_time": "09:45"
-#     },
-#     {
-#         "bus_id": 256,
-#         "stop_id": 6,
-#         "stop_name": "Sunset Boulevard",
-#         "next_stop": 7,
-#         "stop_type": "",
-#         "a_time": "09:59"
-#     },
-#     {
-#         "bus_id": 256,
-#         "stop_id": 7,
-#         "stop_name": "Sesame Street",
-#         "next_stop": "0",
-#         "stop_type": "F",
-#         "a_time": "10:12"
-#     },
-#     {
-#         "bus_id": 512,
-#         "stop_id": 4,
-#         "stop_name": "Bourbon Street",
-#         "next_stop": 6,
-#         "stop_type": "S",
-#         "a_time": "08:13"
-#     },
-#     {
-#         "bus_id": "512",
-#         "stop_id": 6,
-#         "stop_name": "Sunset Boulevard",
-#         "next_stop": 0,
-#         "stop_type": 5,
-#         "a_time": "08:16"
-#     }
-# ]'''
-
-
 def integer(k_j, k_e, v_j):
     return k_j == k_e and isinstance(v_j, int)
 
@@ -229,7 +138,12 @@ def time(k_j, k_e, v_j):
            and val_j[2] == ':' and int(val_j[0]) < 6 and int(val_j[3]) < 6
 
 
+err_dict = {'bus_id': [0, 'int'], 'stop_id': [0, 'int'], 'stop_name': [0, 'str'], 'next_stop': [0, 'int'],
+            'stop_type': [0, 'char'], 'a_time': [0, 'time']}
+
+s = input()
 json_dict = loads(s)
+
 for i in range(len(json_dict)):
     for j, e in zip(json_dict[i].items(), err_dict.items()):
         key_e, val_e, key_j, val_j = *e, *j
