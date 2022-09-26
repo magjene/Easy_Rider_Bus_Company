5220f30c3c8f45812d744b206e4fa70fe7a1f1d5
magjene <pdfjene@mail.ru>
2022-09-27 00:03:42 +0300

    Fulfilled #5

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -172,46 +172,29 @@ Finish stops: 2 ['Sesame Street', 'Sunset Boulevard']
 
 
 from json import loads
-import re
 
 
-err_dict = {'bus_id': [0, 'int'],
-            'stop_id': [0, 'int'],
-            'stop_name': [0, r'\A[A-Z].*[a-z] (Road|Avenue|Boulevard|Street)\Z'],
-            'next_stop': [0, 'int'],
-            'stop_type': [0, r'\A[S|O|F]?\Z'],
-            'a_time': [0, r'\A([0-1][0-9]|2[0-3]):[0-6][0-9]\Z']}
+bus_stop = {'S': 0, 'T': 0, 'F': 0}
+bus_s, bus_t, bus_f = [], [], []
 
-bus_stop = {'S': 0, 'F': 0}
-
-s = '[{"bus_id": 128, "stop_id": 1, "stop_name": "Prospekt Avenue", "next_stop": 3, "stop_type": "S", "a_time": "08:12" }]'
 # s = '[{"bus_id": 128, "stop_id": 1, "stop_name": "Prospekt Avenue", "next_stop": 3, "stop_type": "S", "a_time": "08:12" }, {"bus_id": 128, "stop_id": 3, "stop_name": "Elm Street", "next_stop": 5, "stop_type": "", "a_time": "08:19" }, {"bus_id": 128, "stop_id": 5, "stop_name": "Fifth Avenue", "next_stop": 7, "stop_type": "O", "a_time": "08:25" }, {"bus_id": 128, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "08:37" }, {"bus_id": 512, "stop_id": 4, "stop_name": "Bourbon Street", "next_stop": 6, "stop_type": "", "a_time": "08:13" }, {"bus_id": 512, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 0, "stop_type": "F", "a_time": "08:16" }]'
 # s = '[{"bus_id": 128, "stop_id": 1, "stop_name": "Prospekt Avenue", "next_stop": 3, "stop_type": "S", "a_time": "08:12" }, {"bus_id": 128, "stop_id": 3, "stop_name": "Elm Street", "next_stop": 5, "stop_type": "", "a_time": "08:19" }, {"bus_id": 128, "stop_id": 5, "stop_name": "Fifth Avenue", "next_stop": 7, "stop_type": "O", "a_time": "08:25" }, {"bus_id": 128, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "08:37" }, {"bus_id": 256, "stop_id": 2, "stop_name": "Pilotow Street", "next_stop": 3, "stop_type": "S", "a_time": "09:20" }, {"bus_id": 256, "stop_id": 3, "stop_name": "Elm Street", "next_stop": 6, "stop_type": "", "a_time": "09:45" }, {"bus_id": 256, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 7, "stop_type": "", "a_time": "09:59" }, {"bus_id": 256, "stop_id": 7, "stop_name": "Sesame Street", "next_stop": 0, "stop_type": "F", "a_time": "10:12" }, {"bus_id": 512, "stop_id": 4, "stop_name": "Bourbon Street", "next_stop": 6, "stop_type": "S", "a_time": "08:13" }, {"bus_id": 512, "stop_id": 6, "stop_name": "Sunset Boulevard", "next_stop": 0, "stop_type": "F", "a_time": "08:16" }]'
-# s = input()
+s = input()
 json_list = loads(s)
 for json_dict in json_list:
     if json_dict['stop_type'] == 'S':
-        bus_stop[json_dict['stop_type']] += 1
+        bus_stop['S'] += 1
+        bus_s.append(json_dict['stop_name'])
+    elif json_dict['stop_type'] == 'T' or json_dict['stop_type'] == '':
+        bus_stop['T'] += 1
+        bus_t.append(json_dict['stop_name'])
     elif json_dict['stop_type'] == 'F':
-        bus_stop[json_dict['stop_type']] += 1
-    S, F = bus_stop.values()
-    if S > 1:
-        print('S =', S)
-        break
-    elif F > 1:
-        print('F =', F)
+        bus_stop['F'] += 1
+        bus_f.append(json_dict['stop_name'])
+    if bus_stop['F'] > 1 or bus_stop['S'] > 1:
+        print(f'There is no start or end stop for the line: {json_dict["bus_id"]}.')
         break
 else:
-    ...
-
-
-
-# print('Line names and number of stops:')
-# for key, val in bus_stop.items():
-#     print(f'bus_id: {key}, stops: {val}')
-
-'''
-Start stops: 3 ['Bourbon Street', 'Pilotow Street', 'Prospekt Avenue']
-Transfer stops: 3 ['Elm Street', 'Sesame Street', 'Sunset Boulevard']
-Finish stops: 2 ['Sesame Street', 'Sunset Boulevard']
-'''
+    print(f'''Start stops: {bus_stop['S']} {bus_s}
+Transfer stops: {bus_stop['T']} {bus_t}
+Finish stops: {bus_stop['F']} {bus_f}''')
